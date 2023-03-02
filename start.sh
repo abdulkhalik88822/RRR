9f0e13a76f35bf2491c2b8febeb5495a485e7046
abdulkhalik88822 <125821386+abdulkhalik88822@users.noreply.github.com>
2023-03-02 09:26:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/abdulkhalik88822/RRR /RRR
+git clone https://github.com/abdulkhalik88822/RRR.git /RRR
 cd /RRR
 pip3 install -r requirements.txt
 echo "Starting Bot...."
